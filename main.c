1f73572ca0d7ca9c53cdcdeba527627a5863cdb5
LauritzCode <lauritzmazin@gmail.com>
2025-09-03 16:12:43 +0200

    Implement conveyor belt functionality with weight capacity check.

--- main.c
+++ main.c
@@ -1,3 +1,26 @@
+#include <stdbool.h>
 #include <stdio.h>
 
-int main() { return 42; }
+int motorAmount;
+int packageWeight;
+bool conveyorWorks;
+float packageHandle = 5.6;
+
+void submitInfo(int a, int b) {
+  motorAmount = a;
+  packageWeight = b;
+  float weightcapacity = motorAmount * packageHandle;
+
+  if (b <= weightcapacity) {
+    printf("The conveyor belt can carry the packages.");
+  } else {
+    printf("The conveyor belt can't not carry the packages.");
+  }
+
+  return;
+}
+
+int main(void) {
+  submitInfo(5, 1000);
+  return 0;
+}
